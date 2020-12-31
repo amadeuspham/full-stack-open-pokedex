8868abf5a944324deb1a4d832b3d77a2540033e3
Harry Pham <scramblegf@gmail.com>
2020-12-31 15:43:09 +0200

    More e2e test

--- cypress/integration/pokedex.js
+++ cypress/integration/pokedex.js
@@ -4,4 +4,11 @@ describe('Pokedex', function() {
     cy.contains('ivysaur')
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
   })
+  it('one can navigate from front page to a single Pokemon page', function() {
+    cy.visit('http://localhost:5000')
+    cy.contains('ivysaur').click()
+    cy.url().should('include', '/pokemon/ivysaur')
+    cy.contains('chlorophyll')
+    cy.contains('overgrow')
+  })
 })
